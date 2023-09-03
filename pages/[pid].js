82d9a752127cbb578acd9ca5ea106b1ab8f8a47b
Amanullah Aman <amancse.aiub@gmail.com>
2023-09-03 20:07:33 +0600

    Loading paths dynamically

--- pages/[pid].js
+++ pages/[pid].js
@@ -4,9 +4,9 @@ import path from "path";
 
 const ProductDetails = (props) => {
   const { loadedProduct } = props;
-//   if (!loadedProduct) {
-//     return <p>Loading...</p>;
-//   }
+  //   if (!loadedProduct) {
+  //     return <p>Loading...</p>;
+  //   }
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -15,9 +15,21 @@ const ProductDetails = (props) => {
   );
 };
 
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
+}
+
 export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map((product) => product.id);
+
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [{ params: { pid: "p1" } }],
+    paths: pathsWithParams,
     fallback: "blocking",
   };
 }
@@ -27,9 +39,7 @@ export async function getStaticProps(context) {
 
   const productId = params.pid;
 
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
 
