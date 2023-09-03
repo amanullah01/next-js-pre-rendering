4a4f6d070f700a5492e5a403d72f77db4a0550e8
Amanullah Aman <amancse.aiub@gmail.com>
2023-09-03 20:01:43 +0600

    fallback blocking

--- pages/[pid].js
+++ pages/[pid].js
@@ -4,6 +4,9 @@ import path from "path";
 
 const ProductDetails = (props) => {
   const { loadedProduct } = props;
+//   if (!loadedProduct) {
+//     return <p>Loading...</p>;
+//   }
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -14,12 +17,8 @@ const ProductDetails = (props) => {
 
 export async function getStaticPaths() {
   return {
-    paths: [
-      { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
-    ],
-    fallback: false,
+    paths: [{ params: { pid: "p1" } }],
+    fallback: "blocking",
   };
 }
 
